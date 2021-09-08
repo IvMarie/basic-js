7df2a44522319d1f6222dd675ce04170021ca4ae
ivmarie <marie555iv@gmail.com>
2021-09-08 10:58:07 +0400

    feat: implement function

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  if (str.length === 0) return str;
+
+  const result = [];
+  let count = 1;
+
+  str.split('').forEach((el, i, arr) => {
+    if (el === arr[i + 1]) {
+      count++;
+    } else if (count === 1) {
+      result.push(el);
+    } else {
+      result.push(`${count}${el}`);
+      count = 1;
+    }
+  });
+
+  return result.join('');
 }
